217023e5870f0d16262c46a208d187bc8c804e92
christinakopi <xristinakopidaki@gmail.com>
2024-10-17 15:02:52 +0200

    changes in foramt of comments

--- labs/ex03/template/build_polynomial.py
+++ labs/ex03/template/build_polynomial.py
@@ -17,11 +17,9 @@ def build_poly(x, degree):
     array([[1.  , 0.  , 0.  ],
            [1.  , 1.5 , 2.25]])
     """
-    # ***************************************************
     poly = np.zeros([len(x), degree+1])
     for i in range(len(x)):
         for j in range(degree+1):
             poly[i, j] = x[i]**j
 
     return poly
-    # ***************************************************
